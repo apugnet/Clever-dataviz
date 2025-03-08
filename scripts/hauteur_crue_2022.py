49ccac2f339e72ae03e3178b9f1d5a389c21eb95
Jean-Paul Grandgirard <jp.grandgirard@protonmail.com>
2025-03-08 13:24:07 +0100

    plot crue 2022

---
+++ scripts/hauteur_crue_2022.py
@@ -0,0 +1,14 @@
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+current_path =  os.path.dirname(__file__)
+data = pd.read_csv(current_path + "/hauteur_crue_2022.csv")
+sns.catplot(data=data.sort_values("date_heure"), x="date_heure", y='h_ratio', hue="distance")
+
+plt.show();
+
+
+
+
